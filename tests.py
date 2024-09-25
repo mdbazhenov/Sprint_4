bd8ca9953c4bc9d704cac83c90ed69feca42f231
Sergei Bazhenov <mdbazhenov@gmail.com>
2024-09-25 21:36:42 +0200

    fixed after review

--- tests.py
+++ tests.py
@@ -9,6 +9,10 @@ class TestBooksCollector:
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
         assert len(collector.get_books_genre()) == 2
+
+    def test_added_book_no_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
         assert collector.get_book_genre('Гордость и предубеждение и зомби') == ''
 
     @pytest.mark.parametrize('book_name, genre',
@@ -42,16 +46,25 @@ class TestBooksCollector:
         expected_books_genre = {'Таня Гроттер': 'Ужасы'}
         assert collector.get_books_genre() == expected_books_genre
 
-    @pytest.mark.parametrize('book_name, genre',
-    [('Туманность Андромеды', 'Фантастика'), ('Аманность Тундромеды', 'Ужасы')])
-    def test_books_for_children(self, book_name, genre):
+    @pytest.mark.parametrize('book_name, genre', [
+        ('Туманность Андромеды', 'Фантастика'),
+        ('Колобок', 'Мультфильмы')
+    ])
+    def test_books_for_children_allowed(self, book_name, genre):
         collector = BooksCollector()
         collector.add_new_book(book_name)
         collector.set_book_genre(book_name, genre)
-        if genre in collector.genre_age_rating:
-            assert book_name not in collector.get_books_for_children()
-        else:
-            assert book_name in collector.get_books_for_children()
+        assert book_name in collector.get_books_for_children()
+
+    @pytest.mark.parametrize('book_name, genre', [
+        ('Шерлок Холмс', 'Детективы'),
+        ('Дракула', 'Ужасы')
+    ])
+    def test_books_for_children_not_allowed(self, book_name, genre):
+        collector = BooksCollector()
+        collector.add_new_book(book_name)
+        collector.set_book_genre(book_name, genre)
+        assert book_name not in collector.get_books_for_children()
 
     @pytest.mark.parametrize("book_name", [
         ('Чевенгур'),
